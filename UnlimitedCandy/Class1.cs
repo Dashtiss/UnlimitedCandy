22aef253e729057c7360a7e3f900f00fa7065b13
Dashtiss <brandontisserand@outlook.com>
2023-10-28 15:01:55 -0500

    Add project files.

---
+++ UnlimitedCandy/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Scp330;
+using Exiled.Events.Handlers;
+
+namespace UnlimitedCandy
+{
+    public class Class1 : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            Scp330.InteractingScp330 += CandyInteract;
+            base.OnEnabled();
+        }
+
+        public void CandyInteract(InteractingScp330EventArgs ev)
+        {
+            Config config = new Config();
+            if (config.Max == -1)
+            {
+                ev.ShouldSever = false;
+                ev.IsAllowed = true;
+            }
+            if (ev.UsageCount < config.Max)
+            {
+                ev.ShouldSever = false;
+                ev.IsAllowed = true;
+            }
+        }
+    }
+}
---
+++ UnlimitedCandy/Config.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlimitedCandy
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        [Description("How much candy can player get, -1 disables limit")]
+        public int Max { get; set; } = -1;
+    }
+}
